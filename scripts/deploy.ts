c6827fa476f1607ac5f9e2fe59c68ae31e60e302
Insu-Lee <insu.lee@mrhi.or.kr>
2025-03-08 16:23:37 +0900

    1차 erc20

---
+++ scripts/deploy.ts
@@ -0,0 +1,22 @@
+import { ethers } from 'hardhat';
+import { makeAbi } from './abiGenerator';
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  const contractName = ''; // Todo: Deploy하는 컨트랙트의 이름을 입력해주세요
+
+  console.log(`Deploying contracts with the account: ${deployer.address}`);
+
+  const Contract = await ethers.getContractFactory(contractName);
+  const contract = await Contract.deploy(deployer.address);
+
+  await contract.waitForDeployment();
+
+  console.log(`Contract deployed at: ${contract.target}`);
+  await makeAbi(`${contractName}`, contract.target);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
