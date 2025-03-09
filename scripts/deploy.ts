54c8f8b91d27b3dac7cb04b09f6c717898295ac8
Insu-Lee <insu.lee@mrhi.or.kr>
2025-03-09 12:06:06 +0900

    Add: deploy test

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,13 +2,12 @@ import { ethers } from 'hardhat';
 import { makeAbi } from './abiGenerator';
 
 async function main() {
-  const [deployer] = await ethers.getSigners();
   const contractName = 'Mytoken';
 
-  console.log(`Deploying contracts with the account: ${deployer.address}`);
+  console.log(`Deploying contracts`);
 
   const Contract = await ethers.getContractFactory(contractName);
-  const contract = await Contract.deploy(deployer.address);
+  const contract = await Contract.deploy();
 
   await contract.waitForDeployment();
 
